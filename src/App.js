dbbc74410d44793eaccdee63601d349c9521bdca
yuyunfrancis <yuyun.francis@ictuniversity.org>
2022-01-18 22:02:51 +0100

    display photos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { FaSearch } from 'react-icons/fa'
+import Photo from './Photo'
+
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+const mainUrl = `https://api.unsplash.com/photos/`
+const searchUrl = `https://api.unsplash.com/search/photos/`
 
 function App() {
+
+  const [loading, setLoading] = useState(false)
+  const [photos, setPhotos] = useState([])
+
+  const fetchImages = async() => {
+    setLoading(true)
+    let url;
+    url = `${mainUrl}${clientID}`
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      setPhotos(data)
+      setLoading(false)
+    }catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchImages()
+
+  },[])
+
+  useEffect()
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('hello')
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <main>
+        <section className='search'>
+          <form className='search-form'>
+            <input type='text' placeholder='search' className='form-input'/>
+            <button type='submit' className='submit-btn' onClick={handleSubmit}>
+              <FaSearch/>
+            </button>
+          </form>
+        </section>
+        <section className='photos'>
+          <div className='photos-center'>
+            {photos.map((image,index) => {
+              return(
+                  <Photo key={image.id} {...image}/>
+              )
+            })}
+          </div>
+          {loading && <h2 className='loading'>Loading...</h2>}
+        </section>
+      </main>
+  )
+
 }
 
-export default App;
+export default App
